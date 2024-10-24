6ece6d431b032bc5f4268743258d07b58ed96367
lowbacc <skalski.matthis@gmail.com>
2024-10-24 16:20:29 +0200

    Refactor owner_id parameter in Place model constructor

--- app/models/place.py
+++ app/models/place.py
@@ -5,7 +5,7 @@ from .base_model import BaseModel
 
 
 class Place(BaseModel):
-    def __init__(self, title, description, price, latitude, longitude, owner):
+    def __init__(self, title, description, price, latitude, longitude, owner_id):
         super().__init__()
         self.title = title
         self.description = description
